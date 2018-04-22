d9c149cb577f1a9eed62ccf690f55049d5d092ad
mmayye <mmayye@gmail.com>
2018-04-22 16:59:27 +0800

    1

---
+++ core/configini.go
@@ -0,0 +1,150 @@
+package core
+
+import (
+    "io"
+    "strings"
+    "strconv"
+    "os"
+    "bufio"
+    "sync"
+)
+
+// INI文件内容存贮结构
+type ConfigIni struct {
+    // 文件名
+    file       string
+    // 段落内容
+    paragraphs map[string]map[string]string
+    sync       *sync.Mutex
+}
+
+// 创建配置对象
+func NewConfigIni(file string) *ConfigIni {
+    cfg := new(ConfigIni)
+    cfg.paragraphs = make(map[string]map[string]string)
+    cfg.file = file
+    cfg.sync = &sync.Mutex {}
+    cfg.Load()
+    return cfg
+}
+
+// 内容加载
+func (this *ConfigIni) Load() {
+    this.sync.Lock()
+    defer this.sync.Unlock()
+
+    file, err := os.Open(this.file)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    //var paragraph map[string]map[string]string
+    var paragraph string
+    buf := bufio.NewReader(file)
+    for {
+        line, err := buf.ReadString('\n')
+        if err != nil {
+            if err != io.EOF {
+                panic(err)
+            }
+            if len(line) == 0 {
+                break
+            }
+        }
+        lineNS := strings.TrimSpace(line)
+        if strings.HasPrefix(lineNS, "#") || lineNS == "" {
+            continue
+        }
+        switch {
+        case len(lineNS) == 0:
+        case lineNS[0] == '[' && lineNS[len(lineNS)-1] == ']':
+            paragraph = strings.TrimSpace(lineNS[1 : len(lineNS)-1])
+
+            _,exist := this.paragraphs[paragraph]
+            if !exist {
+                this.paragraphs[paragraph] = make(map[string]string)
+            }
+        default:
+            i := strings.IndexAny(lineNS, "=")
+            key := strings.TrimSpace(lineNS[0:i])
+            value := strings.TrimSpace(lineNS[i+1 :])
+            this.paragraphs[paragraph][key] = value
+        }
+    }
+}
+
+// 返回段落集合
+func (this *ConfigIni) ParagraphSet() []string {
+    var keys []string
+    for key := range this.paragraphs {
+        keys = append(keys, key)
+    }
+    return keys
+}
+
+// 返回段落内容
+func (this *ConfigIni) Paragraph(paragraph string) map[string]string {
+    _,exist := this.paragraphs[paragraph]
+    if exist {
+        return this.paragraphs[paragraph]
+    }
+    return nil
+}
+
+// 读取文本值
+func (this *ConfigIni) Get(paragraph string, key string) string {
+    return this.paragraphs[paragraph][key]
+}
+
+// 设置文本值（暂不支持文件保存）
+func (this *ConfigIni) Set(paragraph string, key string, value string) {
+    this.sync.Lock()
+    defer this.sync.Unlock()
+    _,exist := this.paragraphs[paragraph]
+    if !exist {
+        this.paragraphs[paragraph] = make(map[string]string)
+    }
+    this.paragraphs[paragraph][key] = value
+}
+
+// 移除（暂不支持文件保存）
+func (this *ConfigIni) Remove(paragraph string, key string) {
+    this.sync.Lock()
+    defer this.sync.Unlock()
+    _,exist := this.paragraphs[paragraph]
+    if exist {
+        delete(this.paragraphs[paragraph], key)
+    }
+}
+
+// 读取文本值列表
+func (this *ConfigIni) GetList(paragraph string, key string) []string {
+    value :=  this.Get(paragraph, key)
+    if value == "" {
+        return nil
+    }
+    values := strings.Split(value, ",")
+    return values
+}
+
+// 读取整数值
+func (this *ConfigIni) GetInt(paragraph string, key string) (int, error) {
+    s := this.Get(paragraph, key)
+    return strconv.Atoi(s)
+}
+
+// 读取整数值列表
+func (this *ConfigIni) GetIntList(paragraph string, key string) []int {
+    strValues := this.GetList(paragraph, key)
+    var intValues []int
+    for _, s := range strValues {
+        v,err:=strconv.Atoi(s)
+        if err != nil {
+            return nil
+        }
+        intValues=append(intValues,v)
+    }
+    return intValues
+}
+
+
---
+++ core/configproperties.go
@@ -0,0 +1,118 @@
+package core
+
+import (
+    "io"
+    "strings"
+    "strconv"
+    "os"
+    "bufio"
+    "sync"
+)
+
+// 属性文件内容存贮结构
+type ConfigProperties struct {
+    // 文件名
+    file     string
+    // 内容
+    contents map[string]string
+    sync     *sync.Mutex
+}
+
+// 创建配置对象
+func NewConfigProperties(file string) *ConfigProperties {
+    cfg := new(ConfigProperties)
+    cfg.contents = make(map[string]string)
+    cfg.file = file
+    cfg.sync = &sync.Mutex {}
+    cfg.Load()
+    return cfg
+}
+
+// 内容加载
+func (this *ConfigProperties) Load() {
+    this.sync.Lock()
+    defer this.sync.Unlock()
+
+    file, err := os.Open(this.file)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    buf := bufio.NewReader(file)
+    for {
+        line, err := buf.ReadString('\n')
+        if err != nil {
+            if err != io.EOF {
+                panic(err)
+            }
+            if len(line) == 0 {
+                break
+            }
+        }
+        lineNS := strings.TrimSpace(line)
+        if strings.HasPrefix(lineNS, "#") || lineNS == "" {
+            continue
+        }
+
+        i := strings.IndexAny(lineNS, "=")
+        key := strings.TrimSpace(lineNS[0:i])
+        value := strings.TrimSpace(lineNS[i+1 :])
+        this.contents[key] = value
+    }
+}
+
+// 返回Key集合
+func (this *ConfigProperties) KeySet() []string {
+    var keys []string
+    for key := range this.contents {
+        keys = append(keys, key)
+    }
+    return keys
+}
+
+// 读取文本值
+func (this *ConfigProperties) Get(name string) string {
+    return this.contents[name]
+}
+
+// 设置文本值（暂不支持文件保存）
+func (this *ConfigProperties) Set(key string, value string) {
+    this.contents[key] = value
+}
+
+// 移除（暂不支持文件保存）
+func (this *ConfigProperties) Remove(key string) {
+    delete(this.contents, key)
+}
+
+// 读取文本值列表
+func (this *ConfigProperties) GetList(key string) []string {
+    value :=  this.Get(key)
+    if value == "" {
+        return nil
+    }
+    values := strings.Split(value, ",")
+    return values
+}
+
+// 读取整数值
+func (this *ConfigProperties) GetInt(key string) (int,error ) {
+   s := this.Get(key)
+   return strconv.Atoi(s)
+}
+
+// 读取整数值列表
+func (this *ConfigProperties) GetIntList(key string) ([]int) {
+    strValues := this.GetList(key)
+    var intValues []int
+    for _, s := range strValues {
+        v,err:=strconv.Atoi(s)
+        if err != nil {
+            return nil
+        }
+        intValues=append(intValues,v)
+    }
+    return intValues
+}
+
+
