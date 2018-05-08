6a81ee65fb0c7f9b11d78ccccf7ac7e3e93fc2ab
xiayang <xiayang@cn.ibm.com>
2018-05-08 08:41:17 +0800

    Update package description for gear

--- package.go
+++ package.go
@@ -4,4 +4,7 @@
  * license that can be found in the LICENSE file.
  */
 
+// Package gear is a golang framework for enterprise applications.
+// It aims to provide easy-to-use features for who shift to golang from Java or C#,
+// or any new comers to golang.
 package gear
